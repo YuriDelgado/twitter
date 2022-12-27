a21bd5f7bdef7d04f7ba76fa706fa2da2030a833
Yuri Delgado <yuridelgado@Yuris-MacBook-Air.local>
2022-12-26 22:22:52 -0600

    adding tailwind

--- app/javascript/components/tweets/index.jsx
+++ app/javascript/components/tweets/index.jsx
@@ -4,7 +4,16 @@ import ReactDom from "react-dom/client"
 class Tweets extends React.Component {
   render() {
     return(
-      <div>Tweets on react!</div>
+      <div>
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+          <div className="shrink-0">
+          </div>
+          <div>
+            <div className="text-3xl font-medium text-orange-400 italic">ChitChat</div>
+            <p className="font-bold text-2xl text-slate-100">You have a new message!</p>
+          </div>
+        </div>
+      </div>
     )
   }
 }
